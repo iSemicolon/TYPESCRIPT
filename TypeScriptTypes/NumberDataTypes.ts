c805fdb5af7032b214b6ecafd054d2811b82c6c0
Palash Samanta <palash.samanta@cgi.com>
2021-10-22 16:51:36 +0530

    Number Data Types

---
+++ TypeScriptTypes/NumberDataTypes.ts
@@ -0,0 +1,15 @@
+var a=10;
+console.log(typeof(a));
+var a1=10.05;
+console.log(typeof(a1));
+var b:number=20;
+console.log(typeof(b));
+var b1=20;
+console.log(typeof(b1));
+
+function add(num1,num2){
+   var num3=num1+num2;
+   console.log(typeof(num3));
+}
+
+add(2,5);
