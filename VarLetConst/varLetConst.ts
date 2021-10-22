6ab17be105ea3fb150937f5b71d920ea8e8943fe
Palash Samanta <palash.samanta@cgi.com>
2021-10-22 16:04:10 +0530

    Diff Between Var Let Const

---
+++ VarLetConst/varLetConst.ts
@@ -0,0 +1,37 @@
+//console.log(x); //undefined but no error
+var x: string="PALASH";
+console.log(x); //PALASH
+
+/*
+console.log(m); //error
+let m=10;
+console.log(m);
+
+*/
+/*
+function varGreeter(){
+    var a = 10;        
+    var a = 20; //a is replaced
+    console.log(a);
+  }
+  varGreeter();*/
+
+  /*
+  function varGreeter(){
+    let a = 10;        
+   let a = 20; //SyntaxError: 
+   //Identifier 'a' has already been declared
+    console.log(a);
+  }
+  varGreeter();
+
+*/
+
+const h=4;
+h=5; //Error
+
+
+
+
+
+
