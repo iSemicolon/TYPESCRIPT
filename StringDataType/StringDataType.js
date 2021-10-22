449c77cff3eda969e1d0ead66faae758c94725b4
Palash Samanta <palash.samanta@cgi.com>
2021-10-22 17:34:29 +0530

    String Data Type

---
+++ StringDataType/StringDataType.js
@@ -0,0 +1,13 @@
+var str = "PALASH";
+console.log(typeof (str));
+var str1 = 'SAMANTA';
+console.log(typeof (str1));
+var str2 = "Tiya";
+console.log(typeof (str2));
+var str3 = 'Maiti';
+console.log(typeof (str3));
+var output = str2 + " " + str3 + " is Senior Nurse";
+var output1 = str2 + " " + str3 + "  is Senior Nurse"; //on tilt button
+console.log(output);
+console.log(output1);
+//str=2; Error
---
+++ StringDataType/StringDataType.ts
@@ -0,0 +1,22 @@
+var str="PALASH";
+console.log(typeof(str));
+
+var str1='SAMANTA';
+console.log(typeof(str1));
+
+
+var str2:string="Tiya";
+console.log(typeof(str2));
+
+var str3:string='Maiti';
+console.log(typeof(str3));
+
+
+var output: string=str2+" "+str3+" is Senior Nurse";
+var output1: string=`${str2} ${str3}  is Senior Nurse`; //on tilt button
+
+console.log(output);
+console.log(output1);
+
+
+//str=2; Error
