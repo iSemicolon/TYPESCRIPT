0b292db53a28c524212a86c1c7f6822ea2a1f464
Palash Samanta <palash.samanta@cgi.com>
2021-10-22 15:03:44 +0530

    Data Types

---
+++ TypeAnnotations/DataType.js
@@ -0,0 +1,7 @@
+var a = 10;
+var str = "PALASH";
+var isActive = true;
+console.log(a);
+console.log(typeof (a));
+console.log(str);
+console.log(typeof (str));
---
+++ TypeAnnotations/DataType.ts
@@ -0,0 +1,7 @@
+var a: number=10;
+var str: string="PALASH";
+var isActive: boolean=true;
+console.log(a);
+console.log(typeof(a));
+console.log(str);
+console.log(typeof(str));
